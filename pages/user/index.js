7c5722a4694af92a3f65600963cd43a7aea26b63
Mdimasdimyati <androdim888@gmail.com>
2021-11-16 14:22:23 +0700

    Make UI with chakra Ui darkmode

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Button } from "@chakra-ui/button"
 import { Modal, ModalBody, ModalHeader, ModalCloseButton,  ModalContent, ModalOverlay } from '@chakra-ui/modal'
+import { useColorMode } from "@chakra-ui/color-mode"
 
 const UserPage = () => {
     const [isModalShow, setIsModalShow] = useState(false)
@@ -11,8 +12,11 @@ const UserPage = () => {
         setIsModalShow(false)
     }
 
+    const { colorMode, toggleColorMode } = useColorMode()
+
     return (
         <div>
+            <Button colorScheme="blue" onClick={toggleColorMode}>DarkMode</Button>
             <Button colorScheme="blue" onClick={handleOpenModal}>Button</Button>
             <Modal motionPreset="slideInBottom" isOpen={isModalShow} onClose={handleCloseModal}> 
                 <ModalOverlay />
