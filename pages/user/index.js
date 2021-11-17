3ad91e652f7d7f3fae0775706878423f70705f2b
Mdimasdimyati <androdim888@gmail.com>
2021-11-17 15:37:35 +0700

    finish unil update data

--- pages/user/index.js
+++ pages/user/index.js
@@ -13,6 +13,35 @@ import { useFormik } from 'formik';
 const UserPage = () => {
     const [usersData, setUsersData] = useState([])
     const [isModalShow, setModalShow] = useState(false)
+    const [isEdit, setEdit] = useState(false)
+    const [editedId, setEditedId] = useState(null)
+
+    const formik = useFormik({
+        initialValues: {
+        fullName: '',
+        email: '',
+        password: '',
+        role: '',
+        },
+        onSubmit: async (values) => {
+
+            if(isEdit){
+                await updateUser(editedId, values)
+            }else{
+                await createUser(values)
+            }
+            return closeModal();
+        },
+    });
+
+    const showModal = () => {
+        setEdit(false);
+        setModalShow(true)
+    }
+    const closeModal = () => {
+        setModalShow(false)
+        formik.resetForm();
+    }
 
 
     const getUsers = async () => {
@@ -35,9 +64,45 @@ const UserPage = () => {
             setUsersData((prev) => {
                 return [...prev, response.data.user]
             })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-            //untuk reset form
-            formik.resetForm();
+    const getUserById = async (id) => {
+        try {
+
+            setEdit(true);
+
+            const {data} = await axios.get(`http://localhost:5000/users/${id}`)
+            // null police >> data?.user?.fullName sama dengan if(data && data.user && data.user.fullName){//...}
+            formik.setValues({
+                fullName: data?.user?.fullName,
+                email: data?.user?.email,
+                password: data?.user?.password,
+                role: data?.user?.role,
+            })
+
+            setEditedId( data?.user?.id,)
+            setModalShow(true)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const updateUser = async (id, form) => {
+        try {
+            const config = { "content-type" : "Aplication/json",}
+            //proses update data server
+            const {data} = await axios.patch(`http://localhost:5000/users/${id}`, form, config)
+            
+            //untuk menampilkan data baru setelah diupdate
+            const newUpdateUser = usersData.map((user) => user.id === id ? {
+                ...user,
+                ...form
+            } : user);
+            setUsersData(newUpdateUser)
+            closeModal()
         } catch (error) {
             console.log(error);
         }
@@ -47,27 +112,7 @@ const UserPage = () => {
        getUsers();
     }, [])
 
-    const showModal = () => {
-        setModalShow(true)
-    }
-    const closeModal = () => {
-        setModalShow(false)
-    }
-
-    const formik = useFormik({
-        initialValues: {
-        fullName: '',
-        email: '',
-        password: '',
-        role: '',
-        },
-        onSubmit: async (values) => {
-            await createUser(values)
-
-            return closeModal();
-        },
-    });
-
+ 
     return (
         <Container>
             <Button size="lg" colorScheme="facebook" onClick={showModal}>Create</Button>
@@ -81,17 +126,22 @@ const UserPage = () => {
                     </Tr>
                 </Thead>
                 <Tbody>
-                    {usersData.map((users) => (
-                        <Tr key={users.id}>
-                            <Td>{users.fullName}</Td>
-                            <Td>{users.email}</Td>
-                            <Td isNumeric>{users.role}</Td>
+                    {usersData.map((value) => (
+                        <Tr key={value.id}>
+                            <Td>{value.fullName}</Td>
+                            <Td>{value.email}</Td>
+                            <Td isNumeric>{value.role}</Td>
+                            <Td>
+                                <Button colorScheme="whatsapp" my={5} width="100%" type="submit" onClick={() => {getUserById(value.id)}}>
+                                    Edit
+                                </Button>
+                            </Td>
                         </Tr>
                     ))}
                   
                 </Tbody>
             </Table>
-            <ModalGlobal isOpen= {isModalShow} onClose={closeModal} title="Create User">
+            <ModalGlobal isOpen= {isModalShow} onClose={closeModal} title={isEdit ? "Update Data" : "Create Data"}>
                 <form onSubmit={formik.handleSubmit}>
                     <FormControl id="fullName" isRequired>
                         <FormLabel>Full Name</FormLabel>
@@ -112,7 +162,7 @@ const UserPage = () => {
                             <option>User</option>
                         </Select>
                     </FormControl>
-                     <Button isLoading={formik.isSubmitting} type="submit" colorScheme="whatsapp" my={5} width="100%">Save</Button>
+                     <Button isLoading={formik.isSubmitting} type="submit" colorScheme="whatsapp" my={5} width="100%">{isEdit ? "Update" : "Save"} Data</Button>
                 </form>
             </ModalGlobal>
         </Container>
